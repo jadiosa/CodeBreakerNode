05223162ac99971d852a1191d461bc7e78f370b4
Jonathan Diosa <jadiosa@experimentality.co>
2018-05-12 11:53:44 -0500

    IC example

--- test/test.js
+++ test/test.js
@@ -2,45 +2,46 @@ var assert = require('assert');
 var codeBreak=require('../functionTest/CodeBreaker');
 
 describe('RomanTest',function(){
+  
     describe('codebreakerTest',function(){
             it('its the secret number',function(){
-                assert.equal(codeBreak.isTheSecretNumber("9831"),"XXXX");        
+                assert.equal(codeBreak.isTheSecretNumber("9831"),"XXXX");
             });
             it('almost the secret number',function(){
-                assert.equal(codeBreak.isTheSecretNumber("9832"),"XXX");        
+                assert.equal(codeBreak.isTheSecretNumber("9832"),"XXX");
             });
             it('desorder secret number',function(){
-                assert.equal(codeBreak.isTheSecretNumber("1389"),"----");        
+                assert.equal(codeBreak.isTheSecretNumber("1389"),"----");
             });
             it('try again',function(){
-                assert.equal(codeBreak.isTheSecretNumber("2456"),"");        
+                assert.equal(codeBreak.isTheSecretNumber("2456"),"");
             });
             it('a half order secret number',function(){
-                assert.equal(codeBreak.isTheSecretNumber("9138"),"XX--");        
+                assert.equal(codeBreak.isTheSecretNumber("9138"),"XX--");
             });
             it('not a number',function(){
-                assert.equal(codeBreak.isTheSecretNumber("xxxx"),"Fake number");        
+                assert.equal(codeBreak.isTheSecretNumber("xxxx"),"Fake number");
             });
             it('not a number either',function(){
-                assert.equal(codeBreak.isTheSecretNumber("1xx1"),"Fake number");        
+                assert.equal(codeBreak.isTheSecretNumber("1xx1"),"Fake number");
             });
             it('one digit less',function(){
-                assert.equal(codeBreak.isTheSecretNumber("123"),"Invalid size");        
+                assert.equal(codeBreak.isTheSecretNumber("123"),"Invalid size");
             });
             it('four digit more',function(){
-                assert.equal(codeBreak.isTheSecretNumber(""),"Invalid size");        
+                assert.equal(codeBreak.isTheSecretNumber(""),"Invalid size");
             });
             it('two digit more',function(){
-                assert.equal(codeBreak.isTheSecretNumber("123456"),"Invalid size");        
+                assert.equal(codeBreak.isTheSecretNumber("123456"),"Invalid size");
             });
             it('digit repeated',function(){
-                assert.equal(codeBreak.isTheSecretNumber("1231"),"number with digits repeated");        
+                assert.equal(codeBreak.isTheSecretNumber("1231"),"number with digits repeated");
             });
             it('digit repeated',function(){
-                assert.equal(codeBreak.isTheSecretNumber("1223"),"number with digits repeated");        
+                assert.equal(codeBreak.isTheSecretNumber("1223"),"number with digits repeated");
             });
             it('digit repeated',function(){
-                assert.equal(codeBreak.isTheSecretNumber("1233"),"number with digits repeated");        
+                assert.equal(codeBreak.isTheSecretNumber("1233"),"number with digits repeated");
             });
         });
 
